1b039918e8d3d05fb8d6d0b90a7268843f2af0da
Gavin <88601252+GavinHillDev@users.noreply.github.com>
2025-11-23 23:31:46 -0500

    Fixed endpoint when User logs in

--- src/app/Components/dashboard.ts
+++ src/app/Components/dashboard.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { RouterOutlet } from '@angular/router';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { error } from 'console';
+import { Console, error } from 'console';
 @Component({
   selector: 'dashboard',
   standalone: true,
@@ -11,23 +11,53 @@ import { error } from 'console';
   templateUrl: '../Html/dashboard.html',
   //styleUrl: './app.css'
 
-})
-export class DashboardComponent {
+}) export class DashboardComponent {
 
   private http = inject(HttpClient);
   private apiUrl = 'http://localhost:5113/api/Users/me';
-
-  checkForUser() {
+  userName: string = "";
+  user = {
+    Username: ""
+  };
+  ngOnInit() {
     const token = localStorage.getItem("token");
     console.log("TOKEN:", token);
-    const headers = new HttpHeaders( {
+    const headers = new HttpHeaders({
       'Authorization': `Bearer ${token}`
     });
 
-    this.http.get(this.apiUrl, {headers}).subscribe({
-      next: res => { console.log(res) }
+    console.log("Calling:", this.apiUrl);
+    console.log("Header:", headers.get("Authorization"));
+    this.http.get<Object>(this.apiUrl, { headers, withCredentials: true }).subscribe({
+      next: res => {
+        console.log(res['username' as keyof Object])
+        this.userName = res['username' as keyof Object].toString();
+      }
       , error: err => console.error('Failed to Login', err)
     })
   }
+
+
+
+  //checkForUser() {
+  //  console.log("CHECK RUN");
+  //  const token = localStorage.getItem("token");
+  //  console.log("TOKEN:", token);
+  //  const headers = new HttpHeaders({
+  //    'Authorization': `Bearer ${token}`
+  //    });
+    
+  //  console.log("Calling:", this.apiUrl);
+  //  console.log("Header:", headers.get("Authorization"));
+  //  this.http.get(this.apiUrl, { headers, withCredentials: true }).subscribe({
+  //    next: res => { console.log(res) }
+  //    , error: err => console.error('Failed to Login', err)
+  //  })
+
+  }
+
+ 
   
 }
+
+//Research JwtTokens & http / https to figure out why get is not working // Log token in backend 
