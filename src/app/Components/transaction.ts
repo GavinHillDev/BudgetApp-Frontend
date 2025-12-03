8269fbf4eaa87a7bab2f0eec6fea77b4be5865cd
Gavin <88601252+GavinHillDev@users.noreply.github.com>
2025-12-02 23:51:25 -0500

    User Finished, Transaction Start

--- src/app/Components/transaction.ts
+++ src/app/Components/transaction.ts
@@ -19,6 +19,46 @@ import { AuthService, User } from '../Services/login.service';
 
 export class TransactionComponent {
   constructor(private authService: AuthService) { }
+  form = new FormGroup({
+    TransactionName: new FormControl(''),
+    TransactionPrice: new FormControl(''),
+    TransactionCategory: new FormControl(''),
+    TransactionDate: new FormControl('')
+  });
 
+  loggeduser: User | null = null;
+
+  userExists = false; //Alert For SignUpCheck
+  userExistsInput = ''; //Input Signup
+  private router = inject(Router);
+  private http = inject(HttpClient);
+  private apiUrl = 'http://localhost:5113/api/Transactions';
+  NgOnInit() {
+    this.authService.user$.subscribe(loggeduser => {
+      this.loggeduser = loggeduser;
+      //  this.user = this.authService.currentUser;
+    });
+  }
+
+  CreateTransaction() {
+    const newTransaction = {
+      TransactionName: this.form.get('TransactionName')?.value,
+      TransactionPrice: this.form.get('TransactionPrice')?.value,
+      TransactionCategory: this.form.get('TransactionCategory')?.value,
+      TransactionDate: this.form.get('TransactionDate')?.value
+    };
+    this.http.post(`${this.apiUrl}/createtransaction`, newTransaction).subscribe({
+      next: res => { console.log("Transaction Created") }
+      , error: err => console.error('Failed to create user', err)
+    })
+  }
+
+
+ 
+  //Create Transaction  - Amount, Category, Date, Name
+  //Dropdown category - Option to creat new category
+  //Show last 5 transactions - seperatee page to view all
+  //Option to delete transactions on view page,
+  // Count transaction category spending and show on main page
 
 }
